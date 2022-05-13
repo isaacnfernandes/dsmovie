5447ac5a87ff60f62886123f6f66290418ac1a54
isaacnfernandes <isaacnfernandes@gmail.com>
2022-05-13 17:47:02 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+	
+	@EmbeddedId
+	private ScorePk id = new ScorePk();
+	private Double value;
+	
+	//Construtor
+	public Score() {
+		
+	}
+
+    //Metodos de acesso
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+	
+	public ScorePk getId() {
+		return id;
+	}
+
+	public void setId(ScorePk id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double valeu) {
+		this.value = valeu;
+	}
+	
+	
+
+}
